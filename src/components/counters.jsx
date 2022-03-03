224b680e082c3d5c4f294254b4be5335a0bac0a1
Eder Canada <canada.eder@gmail.com>
2022-03-03 09:42:48 -0800

    clean up

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -18,30 +18,14 @@ class Counters extends Component {
               <th className="border-none sm-font-grey col-5" scope="col-3">
                 PRODUCT
               </th>
-              <th className="border-none sm-font-grey col-5" scope="col-3">QUANTITY</th>
-              <th className="border-none sm-font-grey col-2" scope="col-3">TOTAL</th>
+              <th className="border-none sm-font-grey col-5" scope="col-3">
+                QUANTITY
+              </th>
+              <th className="border-none sm-font-grey col-2" scope="col-3">
+                TOTAL
+              </th>
             </tr>
           </thead>
-          {/* <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody> */}
         </table>
         {this.props.counters.map((counter) => (
           <Counter
